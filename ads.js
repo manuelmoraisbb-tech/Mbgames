774397f93019d2aeaddc8d30749d86350d991218
manuelmoraisbb-tech <manuelmoraisbb@gmail.com>
2026-01-10 15:51:21 +0100

    Add files via upload

---
+++ ads.js
@@ -0,0 +1,12 @@
+(function (koe) {
+  var d = document,
+      s = d.createElement('script'),
+      l = d.scripts[d.scripts.length - 1];
+
+  s.settings = koe || {};
+  s.src = "//swiftvolume.com/buXmVas/d.GRlp0CY-WNcZ/Jeem/9/urZgUslFkaPlThYX3mNdDAU/z/NPjiUOteNHjNct0/N/TfMk2bN/go";
+  s.async = true;
+  s.referrerPolicy = 'no-referrer-when-downgrade';
+
+  l.parentNode.insertBefore(s, l);
+})({});
