0498117f31c036677d4e43615342209bd89002db
manuelmoraisbb-tech <manuelmoraisbb@gmail.com>
2026-01-10 23:58:09 +0100

    Update ads.js

--- ads.js
+++ ads.js
@@ -1,3 +1,4 @@
+// Script 1
 (function (koe) {
   var d = document,
       s = d.createElement('script'),
@@ -5,6 +6,21 @@
 
   s.settings = koe || {};
   s.src = "//swiftvolume.com/buXmVas/d.GRlp0CY-WNcZ/Jeem/9/urZgUslFkaPlThYX3mNdDAU/z/NPjiUOteNHjNct0/N/TfMk2bN/go";
+  s.async = true;
+  s.referrerPolicy = 'no-referrer-when-downgrade';
+
+  l.parentNode.insertBefore(s, l);
+})({});
+
+
+// Script 2
+(function (kgk) {
+  var d = document,
+      s = d.createElement('script'),
+      l = d.scripts[d.scripts.length - 1];
+
+  s.settings = kgk || {};
+  s.src = "//embarrassed-bridge.com/c.DP9Q6sbT2/5BlsSGWXQb9bNnjUcJ0-NvToY/z/Nuy_0n2pNKz/Q/1UNvjXM/4O";
   s.async = true;
   s.referrerPolicy = 'no-referrer-when-downgrade';
 
